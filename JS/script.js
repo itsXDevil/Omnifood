7fdd66251cebd3cd820be6717d6d380d5c5d29f9
Moein Khazaei <77985536+itsXDevil@users.noreply.github.com>
2021-03-20 17:29:03 +0100

    Finishing my sticky navigatioin using jQuery and learn how to give som animation and scrolling effects

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,31 @@
 $(document).ready (function() {
 
+    $('.js--section-features').waypoint(function(direction) {
+        if(direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '150px'
+      });    
+
+   
+
+
+
+
+
+
+/*
+
+var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+
+*/
 
 
 });
